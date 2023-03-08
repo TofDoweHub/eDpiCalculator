294052a6bd62f4fd8ddb4cd4c2388fdee04e4aad
TofDowe <68544755+TofDoweHub@users.noreply.github.com>
2023-03-08 18:23:38 +0500

    Add files via upload

---
+++ ConsoleApp12/ConsoleApp12/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			while (true)
+			{
+				float dpi, eDpi;
+				float sensitivity;
+
+				Console.WriteLine("Введите ваш dpi:");
+				dpi = Convert.ToSingle(Console.ReadLine());
+				Console.WriteLine("Введите вашу сенсу:");
+				sensitivity = Convert.ToSingle(Console.ReadLine());
+				eDpi = dpi * sensitivity;
+
+				Console.WriteLine($"Ваш eDpi: {eDpi}");
+				Console.ReadLine();
+			}
+		}
+	}
+}
